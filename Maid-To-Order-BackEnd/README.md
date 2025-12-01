dc4b614db9be252d355060807914694f8a4605e4
Juan Cerda <ju.cerdar@duocuc.cl>
2025-12-01 17:42:30 -0300

    OBRevert "Jakarta implementado y correcciones menores"
    This reverts commit ec4b45fc572f52d33af2fd340a86a3f5cc0c7be5.
    
    Se revertio push erroneo

--- Maid-To-Order-BackEnd/README.md
+++
@@ -1,2 +0,0 @@
-# Maid-To-Order-BackEnd
-Repositorio que almacena el back end especializado en maid to order 
--- src/main/java/com/seedOrder/Maid_To_Order_Backend/repository/OrderItemRepository.java
+++
@@ -1,9 +0,0 @@
-package com.seedOrder.Maid_To_Order_Backend.repository;
-
-import com.seedOrder.Maid_To_Order_Backend.model.OrderItem;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface OrderItemRepository extends JpaRepository<OrderItem, Long> {
-}
